Finish softmax portion. Fix small Python2 vs Python3 bug in features portion

assignment1/cs231n/classifiers/softmax.py
@@ -23,17 +23,23 @@ def softmax_loss_naive(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  for i in xrange(num_train):
+    scores = X[i].dot(W)
+    scores -= np.max(scores)
+    likelihood = np.exp(scores)/np.sum(np.exp(scores))
+    loss += -np.log(likelihood[y[i]])
+    dW[:,y[i]] -= X[i,:]
+    for j in xrange(num_classes):
+      dW[:,j] += X[i,:]*likelihood[j]
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+  loss /= num_train
+  dW /= num_train
+    
+  # Add regularization to the loss.
+  loss += reg * np.sum(W * W)
+  dW += 2*reg*W
 
   return loss, dW
 
@@ -45,19 +51,27 @@ def softmax_loss_vectorized(W, X, y, reg):
   Inputs and outputs are the same as softmax_loss_naive.
   """
   # Initialize the loss and gradient to zero.
+  num_train = X.shape[0]
   loss = 0.0
-  dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+  scores = X.dot(W)
+  scores -= np.amax(scores,axis=1)[:,None]
+  exp_scores = np.exp(scores)
+  likelihood = exp_scores/np.sum(exp_scores,axis=1)[:,None]
+  loss = np.sum(-np.log(likelihood[range(num_train),y]))
+    
+  coeffs = likelihood.copy()
+  coeffs[range(num_train),y] -= 1
+  dW = (X.T).dot(coeffs)
+
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss.
+  loss += reg*np.sum(W * W)
+  dW += 2*reg*W
 
   return loss, dW
 